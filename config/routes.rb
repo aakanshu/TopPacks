fa4900cc23c1c94c91da016b50ec3f9f4f8c0320
aakanshu <aakanshu@chronuscorp.com>
2017-12-30 20:24:10 +0530

    Submitting all the changes for the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  match '/' => 'repositories#index', :as => :root, via: :get
+  match '/create' => 'repositories#create', as: :create, via: :post
+  match '/show' => 'repositories#show', as: :show, via: :get
+  match '/packages' => 'repositories#packages', as: :packages, via: :get
+  resources :repositories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
